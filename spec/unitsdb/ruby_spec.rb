96170f51fbcef12fb0f488f5dd4f6d4d3d18a2af
suleman-uzair <suleman@workytical.com>
2024-12-16 18:37:24 +0500

    fixed failing spec and console file

--- spec/unitsdb/ruby_spec.rb
+++ spec/unitsdb/ruby_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Unitsdb do
     expect(Unitsdb::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
+  # it "does something useful" do
+  #   expect(false).to eq(true)
+  # end
 end

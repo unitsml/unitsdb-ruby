f4642a67993fd4b4a527d3041cbf3c45f27ea264
Ronald Tse <ronald.tse@ribose.com>
2025-03-22 11:17:59 +0800

    chore: add unit_systems spec

---
+++ spec/unitsdb/unit_systems_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require_relative "../../lib/unitsdb/unit_systems"
+
+RSpec.describe Unitsdb::UnitSystems do
+  file_path = File.join(__dir__, "../fixtures/unitsdb/unit_systems.yaml")
+
+  it "parses the unit systems collection" do
+    raw_string = IO.read(file_path)
+    parsed = described_class.from_yaml(raw_string)
+    generated = parsed.to_yaml
+
+    expect(generated).to be_yaml_equivalent_to(raw_string)
+  end
+end

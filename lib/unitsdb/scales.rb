b8b86c9b9f31641d8d2d64defc9b3cb99f4fd212
Ronald Tse <ronald.tse@ribose.com>
2025-04-09 20:16:01 +0800

    feat: add scales model

---
+++ lib/unitsdb/scales.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require_relative "scale"
+
+module Unitsdb
+  class Scales < Lutaml::Model::Serializable
+    # model Config.model_for(:Scale)
+    attribute :schema_version, :string
+    attribute :scales, Scale, collection: true
+  end
+end

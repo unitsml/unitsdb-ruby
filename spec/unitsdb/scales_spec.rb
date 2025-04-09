0662b2f34412d256565abf1b7f0476a9372986e9
Ronald Tse <ronald.tse@ribose.com>
2025-04-09 20:16:01 +0800

    feat: add scales model

---
+++ spec/unitsdb/scales_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+RSpec.describe Unitsdb::Scales do
+  file_path = File.join(__dir__, "../fixtures/unitsdb/scales.yaml")
+
+  it "parses the scale collection from the new array structure" do
+    raw_string = IO.read(file_path)
+    parsed = described_class.from_yaml(raw_string)
+    generated = parsed.to_yaml
+
+    # puts generated
+    # puts raw_string
+    expect(generated).to be_yaml_equivalent_to(raw_string)
+  end
+end

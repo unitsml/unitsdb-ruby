92deb92620b1980edcc45561afc34f2358931db1
Ronald Tse <ronald.tse@ribose.com>
2026-04-02 20:39:27 +0900

    Add symbolid and prefix methods to PrefixReference

--- lib/unitsdb/prefix_reference.rb
+++ lib/unitsdb/prefix_reference.rb
@@ -4,5 +4,13 @@ module Unitsdb
   class PrefixReference < Identifier
     attribute :id, :string
     attribute :type, :string
+
+    def symbolid
+      prefix.symbols.first.ascii
+    end
+
+    def prefix
+      @prefix ||= Unitsdb.prefixes.find_by_id(id)
+    end
   end
 end

bd6ce92654457701200afd61b03a8e25397785a4
Ronald Tse <ronald.tse@ribose.com>
2026-04-03 20:38:40 +0900

    chore: remove unused methods

--- lib/unitsdb/prefix_reference.rb
+++ lib/unitsdb/prefix_reference.rb
@@ -4,34 +4,5 @@ module Unitsdb
   class PrefixReference < Identifier
     attribute :id, :string
     attribute :type, :string
-
-    def symbolid
-      prefix_obj = lookup_prefix
-      prefix_obj&.symbols&.first&.ascii
-    end
-
-    def base
-      prefix&.base
-    end
-
-    def power
-      prefix&.power
-    end
-
-    def prefix
-      @prefix ||= lookup_prefix
-    end
-
-    private
-
-    def lookup_prefix
-      return nil unless Unitsdb.database
-
-      Unitsdb.database.prefixes.find do |p|
-        p.identifiers.any? { |i| i.id == id }
-      end
-    rescue StandardError
-      nil
-    end
   end
 end

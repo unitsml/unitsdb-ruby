ff9aa9f13db8757e09965d41aa43746e3482cb57
suleman-uzair <suleman@workytical.com>
2026-04-08 20:53:56 +0500

    refactor database loading and tighten YAML validation

--- spec/unitsdb/database_spec.rb
+++ spec/unitsdb/database_spec.rb
@@ -1,7 +1,13 @@
 # frozen_string_literal: true
 
+require "fileutils"
+require "tmpdir"
+
 RSpec.describe Unitsdb::Database do
-  dir_path = File.join(__dir__, "../../data/")
+  let(:dir_path) { File.expand_path("../../data", __dir__) }
+  let(:database_files) do
+    %w[prefixes.yaml dimensions.yaml units.yaml quantities.yaml unit_systems.yaml]
+  end
 
   it "parses the full unitsdb database" do
     parsed = described_class.from_db(dir_path)
@@ -31,5 +37,54 @@ RSpec.describe Unitsdb::Database do
     # puts generated
     # puts raw_string
     expect(generated).to be_yaml_equivalent_to(combined_yaml)
+  end
+
+  it "does not write to stdout during a normal load" do
+    original_debug = ENV.fetch("DEBUG", nil)
+    ENV.delete("DEBUG")
+
+    output = capture_output { described_class.from_db(dir_path) }
+
+    expect(output[:output]).to eq("")
+  ensure
+    ENV["DEBUG"] = original_debug
+  end
+
+  it "raises a helpful error when a database YAML file is not a mapping" do
+    Dir.mktmpdir do |tmpdir|
+      copy_database_files(tmpdir)
+      File.write(File.join(tmpdir, "units.yaml"), ["invalid"].to_yaml)
+
+      expect do
+        described_class.from_db(tmpdir)
+      end.to raise_error(
+        Unitsdb::Errors::DatabaseFileInvalidError,
+        /Invalid YAML structure in units\.yaml: expected a mapping/,
+      )
+    end
+  end
+
+  it "raises a helpful error when a collection key is missing" do
+    Dir.mktmpdir do |tmpdir|
+      copy_database_files(tmpdir)
+
+      units_file = File.join(tmpdir, "units.yaml")
+      units_hash = YAML.safe_load_file(units_file)
+      units_hash.delete("units")
+      File.write(units_file, units_hash.to_yaml)
+
+      expect do
+        described_class.from_db(tmpdir)
+      end.to raise_error(
+        Unitsdb::Errors::DatabaseFileInvalidError,
+        /Missing units collection in units\.yaml/,
+      )
+    end
+  end
+
+  def copy_database_files(target_dir)
+    database_files.each do |filename|
+      FileUtils.cp(File.join(dir_path, filename), File.join(target_dir, filename))
+    end
   end
 end
